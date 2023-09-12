b0685903715101c8d90902bc734a1b6c8fb4bc6d
reginaldopsilvaf <126976300+reginaldopsilvaf@users.noreply.github.com>
2023-09-12 15:09:22 -0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,30 +1,72 @@
-import os
-import tifffile
-import rasterio
-from torch.utils.data import Dataset
+from os.path import splitext
+from os import listdir
 import numpy as np
+from glob import glob
+import torch
+from torch.utils.data import Dataset
+import logging
+from osgeo import gdal
+from libtiff import TIFF
 
-class DesflorestDataset(Dataset):
-    def __init__(self, image_dir, mask_dir, transform=None):
-        self.image_dir = image_dir
-        self.mask_dir = mask_dir
-        self.transform = transform
-        self.images = os.listdir(image_dir)
+class BasicDataset(Dataset):
+    def __init__(self, imgs_dir, masks_dir, scale=1, mask_suffix='_mask', tif=True):
+        self.imgs_dir = imgs_dir
+        self.masks_dir = masks_dir
+        self.scale = scale
+        self.mask_suffix = mask_suffix
+        self.tif = tif
+        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
 
-    def __len__(self): 
-        return len(self.images)
+        self.ids = [splitext(file)[0] for file in listdir(imgs_dir)
+                    if not file.startswith('.')]
+        logging.info(f'Creating dataset with {len(self.ids)} examples')
 
-    def __getitem__(self, index):
-        img_path = os.path.join(self.image_dir, self.images[index])
-        mask_path = os.path.join(self.mask_dir, self.images[index].replace(".tif", "_mask.tif"))
-        with rasterio.open(img_path) as src:
-            image = np.moveaxis(src.read(), 0, -1)
-        mask = tifffile.imread(mask_path)
-        mask[mask == 255] = 1
+    def __len__(self):
+        return len(self.ids)
 
-        if self.transform is not None:
-            augmentations = self.transform(image=image, mask=mask)
-            image = augmentations["image"]
-            mask = augmentations["mask"]
+    @classmethod
+    def preprocess(cls, cv2_img, scale):
+        w = cv2_img.shape[0]
+        h = cv2_img.shape[1]
+        newW, newH = int(scale * w), int(scale * h)
+        assert newW > 0 and newH > 0, 'Scale is too small'
+        img_nd = np.array(cv2_img)
+        if len(cv2_img.shape) > 2:
+            img_nd = img_nd.transpose((2, 0, 1))
+            if img_nd.max() > 1:
+                img_nd = img_nd / 255
+        return img_nd
 
-        return image, mask
+    def __getitem__(self, i):
+        idx = self.ids[i]
+        mask_file = glob(self.masks_dir + '\\' + idx + '*.tif')
+        img_file = glob(self.imgs_dir + '\\' + idx + '*.tif')
+
+        assert len(mask_file) == 1, \
+            f'Either no mask or multiple masks found for the ID {idx}: {mask_file}'
+        assert len(img_file) == 1, \
+            f'Either no image or multiple images found for the ID {idx}: {img_file}'
+
+        mask = gdal.Open(mask_file[0], gdal.GA_ReadOnly)
+        img = gdal.Open(img_file[0], gdal.GA_ReadOnly)
+
+        # # Transpose image from (bands, rows, columns) to (rows, columns, bands)
+        img = img.ReadAsArray()
+        img = np.transpose(img, (1, 2, 0))
+        mask = mask.ReadAsArray()
+
+        #mask = TIFF.open(mask_file[0], mode='r').read_image()
+        #img = TIFF.open(img_file[0], mode='r').read_image()
+
+        img = self.preprocess(img, self.scale)
+        mask = self.preprocess(mask, self.scale)
+        # print(mask//80)
+
+        return {
+            'image': torch.from_numpy(img).type(torch.float32),
+            'mask': torch.from_numpy(mask).type(torch.uint8)
+        }
+
+class CarvanaDataset(BasicDataset):
+    def __init__(self, imgs_dir, masks_dir, scale=1, tif=False):
+        super().__init__(imgs_dir, masks_dir, scale, mask_suffix='_mask', tif=tif)
